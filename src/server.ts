4bca7b96fb219d149c056de0de42fc73b0ea3b8e
Juliano <julianohbl@hotmail.com>
2025-07-28 18:37:51 -0300

    feat: adiciona estrutura inicial do projeto

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/hello", () => {
+  return { message: "Hello, Juliano!" };
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("Server is running on http://localhost:3333");
+  });
