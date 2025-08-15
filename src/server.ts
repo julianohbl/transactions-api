2e4dfd86f19cf5e63a468927636ce6ac3bc1ea93
Juliano <julianohbl@hotmail.com>
2025-08-14 21:10:09 -0300

    fix: ajuste na config de porta para deploy no Render

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,9 @@ import { env } from "./env";
 
 app
   .listen({
+    host: "0.0.0.0",
     port: env.PORT,
   })
   .then(() => {
-    console.log("Server is running on http://localhost:3333");
+    console.log(`Server is running on http://0.0.0.0:${env.PORT}`);
   });
